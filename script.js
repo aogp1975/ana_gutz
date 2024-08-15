035afb9cb27ea9446e88591d6f67d7873d87d582
aogp1975 <2193038392@correo.ler.uam.mx>
2024-08-15 08:37:42 -0600

    cambios

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log('Este es mi portafolio.')
+
+//*barrita*/
+var dropdown = document.getElementsByClassName("botab");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown [i].addEventListener("click",function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    
+    });
+}
