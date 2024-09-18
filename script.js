74d03e05e84bff066d5b315e8745b73b754ae4a5
aogp1975 <2193038392@correo.ler.uam.mx>
2024-09-18 15:13:57 -0600

    agrego fashion y ecos

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ const menu = document.getElementById('menuabb');
 menubtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+  
+
+
+const textoElement = document.getElementById('escribe');
+const texto = textoElement.textContent;
+textoElement.textContent = '';
+
+let index = 0;
+const velocidad = 100; // Velocidad en milisegundos
+
+function escribirTexto() {
+    if (index < texto.length) {
+        textoElement.textContent += texto.charAt(index);
+        index++;
+        setTimeout(escribirTexto, velocidad);
+    } else {
+        // Reiniciar el índice y limpiar el texto para repetir la animación
+        setTimeout(() => {
+            textoElement.textContent = '';
+            index = 0;
+            escribirTexto();
+        }, 2000); // Tiempo de pausa antes de reiniciar (2 segundos en este caso)
+    }
+}
+
+escribirTexto();
